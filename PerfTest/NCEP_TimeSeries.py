a4c40a02ec1bbd193539eb4f5a9700e91731221a
jreadey@hdfgroup.org <jreadey@hdfgroup.org>
2018-07-12 18:16:01 +0000

    updates for time series perf test

--- PerfTest/NCEP_TimeSeries.py
+++ PerfTest/NCEP_TimeSeries.py
@@ -1,7 +1,7 @@
 import sys
 import random
 import h5pyd
-import numpy as numpy
+import numpy as np
 
 # 
 # Extracts a time series for the NCEP dataset
@@ -24,13 +24,24 @@ if len(sys.argv) > 1:
     if len(sys.argv) > 2:
         x_index = int(sys.argv[2])
     if len(sys.argv) > 3:
-        y_index = int(sys.agrv[3])
+        y_index = int(sys.argv[3])
 
-#f = h5pyd.File("hdf5://shared/NASA/NCEP3/ncep3.he5", "r")
-#tair2m = f["/HDFEOS/GRIDS/NCEP/Data Fields/Tair_2m"]
-#fill_value = tair2m.attrs['_FillValue'][0]
+f = h5pyd.File("hdf5://shared/NASA/NCEP3/ncep3.he5", "r")
+tair2m = f["/HDFEOS/GRIDS/NCEP/Data Fields/Tair_2m"]
+fill_value = tair2m.attrs['_FillValue'][0]
 
 print(f"Getting time series at point ({x_index}, {y_index}) for slices: 0-{end_index}")
-#tseries = tair2m[0:end_index, 350, 600]
+tseries = tair2m[0:end_index, y_index, x_index]
+non_fill = tseries[tseries != fill_value]
+if len(non_fill) == 0:
+    print("no non_fill values returned!")
+    sys.exit(1)
+print("done!")
+tseries_mean = np.mean(non_fill)
+tseries_min = np.min(non_fill)
+tseries_max = np.max(non_fill)
+print(f"Mean: {tseries_mean:.2f} Min: {tseries_min:.2f}  Max: {tseries_max:.2f}")
+        
+
 
 
